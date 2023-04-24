919519a33f7ef2b60b285e006bafda563b36225a
David Howell <david@davidjameshowell.com>
2023-04-24 20:20:43 +0000

    Remove ASG output

--- outputs.tf
+++ outputs.tf
@@ -18,18 +18,6 @@ output "bucket_arn" {
   value = aws_s3_bucket.bucket.arn
 }
 
-output "elb_ip" {
-  value = aws_lb.bastion_lb[0].dns_name
-}
-
-output "elb_arn" {
-  value = aws_lb.bastion_lb[0].arn
-}
-
-output "target_group_arn" {
-  value = aws_lb_target_group.bastion_lb_target_group[0].arn
-}
-
 output "private_instances_security_group" {
   value = aws_security_group.private_instances_security_group.id
 }
